2bb53b6da6f096deba2a991786b0eac73328fa90
Artjom Valdas <artjom.valdas@ut.ee>
2022-01-16 13:46:48 +0200

    moving start and end nodes

--- algorithm.js
+++ algorithm.js
@@ -8,6 +8,9 @@ const NODE_SIZE = 25;
 
 let BOARD_NODES = [];
 let isMouseDown = false;
+let isMovingStartNode = false;
+let isMovingEndNode = false;
+let isDrawingWall = false;
 
 const Status = {
     EMPTY: 'EMPTY',
@@ -62,14 +65,45 @@ class Board {
     addWall(x, y) {
         const node = new Node(Math.floor(x / NODE_SIZE) * NODE_SIZE, Math.floor(y / NODE_SIZE) * NODE_SIZE, NODE_SIZE, Status.WALL);
         if (BOARD_NODES[node.y / NODE_SIZE][node.x / NODE_SIZE].status === Status.EMPTY) {
-            node.drawNode();
-            BOARD_NODES[node.y / NODE_SIZE][node.x / NODE_SIZE] = node
+            this.addNode(node)
         }
-
     }
 
     addNode(node) {
         BOARD_NODES[node.y / NODE_SIZE][node.x / NODE_SIZE] = node;
+        node.drawNode();
+    }
+
+    moveNode(node, x, y) {
+        const old = BOARD_NODES[y][x];
+        const status = node.status;
+        if (old.status !== Status.EMPTY) {
+            return;
+        }
+        old.x = node.x;
+        old.y = node.y;
+        old.status = Status.EMPTY;
+        this.addNode(old);
+        node.x = x * NODE_SIZE;
+        node.y = y * NODE_SIZE;
+        node.status = status;
+        this.addNode(node);
+    }
+
+    startAction(x, y) {
+        if (isMovingStartNode) {
+            this.moveNode(startNode, Math.floor(x / NODE_SIZE), Math.floor(y / NODE_SIZE));
+        } else if (isMovingEndNode) {
+            this.moveNode(finishNode, Math.floor(x / NODE_SIZE), Math.floor(y / NODE_SIZE));
+        } else if (isDrawingWall) {
+            this.addWall(x, y);
+        } else if (BOARD_NODES[Math.floor(y / NODE_SIZE)][Math.floor(x / NODE_SIZE)].status === Status.START) {
+            isMovingStartNode = true;
+        } else if (BOARD_NODES[Math.floor(y / NODE_SIZE)][Math.floor(x / NODE_SIZE)].status === Status.END) {
+            isMovingEndNode = true;
+        } else {
+            isDrawingWall = true;
+        }
     }
 
 }
@@ -85,11 +119,14 @@ canvas.addEventListener('mousedown', function (event) {
 });
 canvas.addEventListener('mousemove', function (event) {
     if (isMouseDown) {
-        board.addWall(event.x - MARGIN, event.y - MARGIN);
+        board.startAction(event.x - MARGIN, event.y - MARGIN);
     }
 });
 canvas.addEventListener('mouseup', function (event) {
     isMouseDown = false;
+    isMovingStartNode = false;
+    isMovingEndNode = false;
+    isDrawingWall = false
 });
 
 function clearBoard() {
@@ -102,6 +139,4 @@ function initializeBoard() {
     board.createBoard();
     board.addNode(startNode);
     board.addNode(finishNode);
-    startNode.drawNode();
-    finishNode.drawNode();
 }
