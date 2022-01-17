a51854c80d8508afd00f91374261037087b81807
Artjom Valdas <artjom.valdas@ut.ee>
2022-01-17 21:59:53 +0200

    Adding maze button

--- algorithm.js
+++ algorithm.js
@@ -166,52 +166,55 @@ function runAlgorithm() {
     const algorithm = document.getElementById('algo-select').value;
     var pathNodes = Asearch(startNode, finishNode);
     console.log(pathNodes.length);
-    for(var i=0;i<pathNodes.length-1;i++){
+    for (let i = 0; i < pathNodes.length - 1; i++) {
         pathNodes[i].status = Status.PATH
         pathNodes[i].drawNode()
     }
-    
+}
+
+function generateMaze() {
+    // TODO: Generate maze
 }
 
 
 // Algorithms
 
-function distance(node1, node2){
-    return Math.pow(Math.pow(node1.x - node2.x,2)+Math.pow(node1.y - node2.y,2),0.5);
+function distance(node1, node2) {
+    return Math.pow(Math.pow(node1.x - node2.x, 2) + Math.pow(node1.y - node2.y, 2), 0.5);
 }
 
-function getNeighbors(node){
+function getNeighbors(node) {
     var result = [];
-    var x = node.x/NODE_SIZE;
-    var y = node.y/NODE_SIZE;
-   
-    if(x >= 1) {
-        result.push(BOARD_NODES[y][x-1]);
+    var x = node.x / NODE_SIZE;
+    var y = node.y / NODE_SIZE;
+
+    if (x >= 1) {
+        result.push(BOARD_NODES[y][x - 1]);
     }
-    if(x < CANVAS_WIDTH / NODE_SIZE) {
-        result.push(BOARD_NODES[y][x+1]);
+    if (x < CANVAS_WIDTH / NODE_SIZE) {
+        result.push(BOARD_NODES[y][x + 1]);
     }
-    if(y >= 1) {
-        result.push(BOARD_NODES[y-1][x]);
+    if (y >= 1) {
+        result.push(BOARD_NODES[y - 1][x]);
     }
-    if(y < CANVAS_HEIGHT / NODE_SIZE) {
-        result.push(BOARD_NODES[y+1][x]);
+    if (y < CANVAS_HEIGHT / NODE_SIZE) {
+        result.push(BOARD_NODES[y + 1][x]);
     }
     return result;
 }
 
-function removeNode(array,node){
-    for(var i=0;i<array.length;i++){
-        if(array[i]==node){
-            array.splice(i,1)
+function removeNode(array, node) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] == node) {
+            array.splice(i, 1)
             break;
         }
     }
 }
 
-function findNode(array,node){
-    for(var i=0;i<array.length;i++){
-        if(array[i]==node){
+function findNode(array, node) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] == node) {
             return true
         }
     }
@@ -219,37 +222,37 @@ function findNode(array,node){
 }
 
 
-function Asearch(start,end)  {
-   
-    var opened   = [];
+function Asearch(start, end) {
+
+    var opened = [];
     var closed = [];
     opened.push(start);
 
-    while(opened.length > 0) {
+    while (opened.length > 0) {
         // Choosing the node with lowest f
         var lowInd = 0;
-        for(var i=0; i<opened.length; i++) {
-            if(opened[i].f < opened[lowInd].f) { 
-                lowInd = i; 
+        for (var i = 0; i < opened.length; i++) {
+            if (opened[i].f < opened[lowInd].f) {
+                lowInd = i;
             }
         }
         var currentNode = opened[lowInd];
-        console.log(currentNode.x,currentNode.y);
+        console.log(currentNode.x, currentNode.y);
         // When endpoint is reached
-        if(currentNode.x === end.x && currentNode.y === end.y) {
+        if (currentNode.x === end.x && currentNode.y === end.y) {
             var curr = currentNode;
             var result = [];
-            while(curr.parent) {
+            while (curr.parent) {
                 result.push(curr);
                 curr = curr.parent;
             }
             return result.reverse();
         }
 
-        
+
         // Removing node from opened
-        
-        removeNode(opened,currentNode)
+
+        removeNode(opened, currentNode)
 
         // Adding node to closed
         closed.push(currentNode);
@@ -258,34 +261,33 @@ function Asearch(start,end)  {
         console.log("Neighbors:")
         console.log(neighbors.length)
         // Adding neighbors to open list and changing f,g and h for them
-        for(var i=0; i<neighbors.length;i++) {
+        for (var i = 0; i < neighbors.length; i++) {
             var neighbor = neighbors[i];
-            if(findNode(closed,neighbor) || neighbor.status == Status.WALL) {
+            if (findNode(closed, neighbor) || neighbor.status === Status.WALL) {
                 continue;
             }
-            
-            
-            var gScore = currentNode.g + 1; 
+
+
+            var gScore = currentNode.g + 1;
             // If this g score is the better than previous for this node
             var gScoreIsBest = false;
 
             // If we visit this node firstly
-            if(!findNode(opened,neighbor)) {
+            if (!findNode(opened, neighbor)) {
                 gScoreIsBest = true;
                 neighbor.h = distance(neighbor, end);
                 opened.push(neighbor);
-            }
-            else if(gScore < neighbor.g) {
+            } else if (gScore < neighbor.g) {
                 gScoreIsBest = true;
             }
 
-            if(gScoreIsBest) {
+            if (gScoreIsBest) {
                 neighbor.parent = currentNode;
                 neighbor.g = gScore;
                 neighbor.f = neighbor.g + neighbor.h;
             }
         }
     }
-    
+
     return [];
-  };
+};
